455eb7281b240490166c40ba102f86e87d1bca60
sola8 <makeresola8@gmail.com>
2021-11-15 17:59:25 -0500

    Added ujson

--- generator.py
+++ generator.py
@@ -1,4 +1,4 @@
-import json
+import ujson as json
 import urllib.request
 
 from config import *
@@ -24,34 +24,51 @@ while True:
         option = input("Input the letter of choice: ").strip().upper()
 
         if (option == "W"):
+            print("Clearing old Pokemon...")
+
+            for player in PCL['players']:
+                if player['tid'] == -2:
+                    player['tid'] = -3
+            
             print("Getting Wild Pokemon...")
             wild_pokemon = ws.batch_get([WILD_POKEMON_RANGE], major_dimension='ROWS')
+
             print("Inputting wild pokemon into the export...")
             wildFill(wild_pokemon, currentYear, PCL)
             break
+
         elif (option == "S"):
             surprise_trade = ws.batch_get([SURPRISE_TRADE], major_dimension='ROWS')
+
             print("-------------------------------------")
             print("Teams:")
             for team in PCL["teams"]:
                 print(f"{team['tid']} - {team['region']} {team['name']}")
             print("-------------------------------------")
+
             tid = input("What team is this pokemon on? Input tid: ")
             surpriseFill(surprise_trade, currentYear, PCL, tid)
             break
+
         elif (option == "P"):
             prof_gift = ws.batch_get([PROFESSOR_GIFT], major_dimension='ROWS')
+
             print("-------------------------------------")
+
             print("Teams:")
             for team in PCL["teams"]:
                 print(f"{team['tid']} - {team['region']} {team['name']}")
+                
             print("-------------------------------------")
+    
             tid = input("What team is this pokemon on? Input tid: ")
             giftFill(prof_gift, currentYear, PCL, tid)
             break
+
         elif (option == "T"):
             print("Getting Spawn Tool Pokemon...")
             spawn_tool = ws.batch_get([SPAWN_TOOL], major_dimension='ROWS')
+            
             print("Inputting Spawn Tool Pokemon into the export...")
             stFill(spawn_tool, currentYear, PCL)
             break
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import json
+import ujson as json
 import gspread
 from config import *
 
